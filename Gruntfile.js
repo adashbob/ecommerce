bf4410596080c1dc9f43fa0936339014311035a6
adashbob <bobodiallobe@gmail.com>
2016-07-12 11:05:15 +0000

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,28 @@
 'use strict';
 
 module.exports = function (grunt) {
-    
+
+    grunt.initConfig({
+        concat: {
+            dist: {
+                src: ['web/js/jquery*.js', 'web/js/bootstrap.js', 'web/js/ajax.js', 'web/bundles/*/js/*.js'],
+                dest: 'web/tmp/built.js',
+            },
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'web/tmp/build-min.js': ['web/tmp/built.js']
+                }
+            }
+        },
+    });
+
+    // Chargement des tâches
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Définition de nouvelles tâches
+    grunt.registerTask('default', ['concat', 'uglify']);
+
 }
